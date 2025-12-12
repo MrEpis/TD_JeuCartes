57936c7357729731d8bed496c9e097bea7c82403
Louis Bedetti <louis.bedetti@etu.unice.fr>
2025-12-12 12:17:16 +0100

    tu le sais.

--- src/cardgame/controller/GameController.java
+++ src/cardgame/controller/GameController.java
@@ -33,40 +33,56 @@ public class GameController {
     public void addPlayer(String playerName) {
         Player player = new Player(playerName);
         playerList.add(player);
+        view.showPlayerName(playerList.indexOf(player), player.getName());
     }
 
     public void startGame() {
         if (gameState == GameState.AddingPlayer) {
             // TODO: Get player names from view
-            gameState = null;
+            gameState = GameState.CardsDealt;
         }
         deck.shuffleDeck();
         for (Player player : playerList) {
             player.addCard(deck.drawCard());
+            view.showFaceDownCardForPlayer(playerList.indexOf(player), player.getName());
         }
         // TODO: Get game state from view
     }
 
     public void flipCards() {
         for (Player player : playerList) {
-            player.getHand().getCards().getFirst().setFaceUp(true);
+            Card card = player.getHand().getCards().getFirst();
+            card.setFaceUp(true);
+            view.showCardForPlayer(playerList.indexOf(player), player.getName(), card.getRank().toString(), card.getSuit().toString());
         }
+        winner = evaluateWinner();
+        view.showWinner(new StringBuilder(winner.getName()));
+        gameState = GameState.WinnerRevealed;
     }
 
-    void evaluateWinner() {
+
+
+    Player evaluateWinner() {
         Card bestCard = new Card(Rank.TWO, Suit.DIAMONDS);
+        Player bestPlayer = new Player("Initial player");
         Card currentCard;
         for (Player player : playerList) {
             currentCard = player.getHand().getCards().getFirst();
             if (currentCard.getRank().value > bestCard.getRank().value) {
                 bestCard = new Card(currentCard.getRank(), currentCard.getSuit());
+                bestPlayer = player;
             } else if (currentCard.getRank().value == bestCard.getRank().value) {
                 if (currentCard.getSuit().value > bestCard.getSuit().value) {
                     bestCard = new Card(currentCard.getRank(), currentCard.getSuit());
+                    bestPlayer = player;
                 }
             }
         }
-        
+        return bestPlayer;
+    }
+
+    public void rebuildDeck() {
+
     }
 
 
